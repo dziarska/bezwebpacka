c61e5306fe9b909a0e4734b3a26a9bd44118193f
DZIARSKA <ZDZIARSKA.KATARZYNA@GMAIL.COM>
2021-09-13 20:55:35 +0200

    drugi comit

--- index.js
+++ index.js
@@ -7,12 +7,136 @@ checkbox.addEventListener('change', ()=>{
     document.body.classList.toggle('dark');
 })
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + arrivalCity.value +"&appid=16757cdb1c5e963cb82b1f47805831e7")
-.then((resp) => resp.json()) // Transform the data into json
-.then(function (data) {
-console.log(data.main.temp - 273)
-let temperature = (data.main.temp-273).toFixed()
-document.getElementById("temperature").innerHTML += `<div id="temperature"> ${temperature}</div>`
+//fetch("https://api.openweathermap.org/data/2.5/weather?q=" + arrivalCity.value +"&appid=16757cdb1c5e963cb82b1f47805831e7")
+//.then((resp) => resp.json()) // Transform the data into json
+//.then(function (data) {
+//console.log(data.main.temp - 273)
+//let temperature = (data.main.temp-273).toFixed()
+//document.getElementById("temperature").innerHTML += `<div id="temperature"> ${temperature}</div>`
+
+//});
+
+const selectArrivalcity = document.querySelector('.arrivalCity1');
+
+selectArrivalcity.addEventListener('change', (event) => {
+  const result = document.querySelector('.arrcity');
+  localStorage.setItem('arrcity', `${event.target.value}`);
+  
+//   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + arrivalCity.value +"&appid=16757cdb1c5e963cb82b1f47805831e7")
+//   .then((resp) => resp.json()) // Transform the data into json
+//   .then(function (data) {
+//   console.log(data.main.temp - 273)
+//   let temperature = (data.main.temp-273).toFixed()
+//   document.getElementById("temperatureArrival").innerHTML += `<div id="temperatureArrival"> ${temperature}</div>`
+//   });
+
+
+
 
 });
+
+const selectDeparturecity = document.querySelector('.departureCity1');
+
+selectDeparturecity.addEventListener('change', (event) => {
+  const res = document.querySelector('.depcity');
+  localStorage.setItem('depcity', `${event.target.value}`);
+//   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + departureCity.value +"&appid=16757cdb1c5e963cb82b1f47805831e7")
+//   .then((resp) => resp.json()) // Transform the data into json
+//   .then(function (data) {
+//   //console.log(data.main.temp - 273)
+//   let temperature = (data.main.temp-273).toFixed()
+//   document.getElementById("temperatureDeparture").innerHTML += `<div id="temperatureDeparture"> ${temperature}</div>`
+
+//   })
+ 
+
+  });
+
+
+  
+ 
+ 
+ 
+
+//const departureTemperature = document.querySelector('.departureCity1');
+ // departureTemperature.addEventListener('change', (event) =>{
+   //  const deptemp = document.querySelector('#temperatureDeparture')
+   // localStorage.setItem('departuretemp',`${event.target.value}`);
+   
+  //})
+
+//const TemperatureCity = document.querySelector('#temperatureDeparture').innerHTML += `<div id="temperatureDeparture"> ${temperature}</div>`
+//console.log(TemperatureCity)
+
+
+const selectOneWay = document.querySelector('#oneway');
+
+selectOneWay.addEventListener('change', (event) => {
+  const selectOneWay = document.querySelector('#oneway');
+  localStorage.setItem('way', `${event.target.value}`);
+});
+
+const selecttwoWay = document.querySelector('#twoway');
+
+selecttwoWay.addEventListener('change', (event) => {
+  const selectOneWay = document.querySelector('#twoway');
+  localStorage.setItem('way', `${event.target.value}`);
+});
+
+const selectDeparturedate = document.querySelector('.departureDate');
+
+selectDeparturedate.addEventListener('change', (event) => {
+  const selectDeparturedate = document.querySelector('.departureDate');
+  localStorage.setItem('data wylotu', `${event.target.value}`);
+});
+
+
+const selectArrivaldate = document.querySelector('.arrivalDate');
+
+selectArrivaldate.addEventListener('change', (event) => {
+  const selectArrivaldate = document.querySelector('.arrivalDate');
+  localStorage.setItem('data powrotu', `${event.target.value}`);
+});
+
+const dateArr = ["0131","0228","0229","0331"]
+
+var date = new Date();
+d = date.getDate();
+m = date.getMonth()+1;
+y = date.getFullYear();
+
+if(d<10){
+    d1='0'+d
+} 
+if(m<10){
+    m='0'+m
+} 
+today =y+"-"+m+"-"+d;
+// console.log(today)
+// todayDate = document.querySelector(".departureDate")
+// todayDate.setAttribute(today)
+document.querySelector(".departureDate").setAttribute("min",today)
+
+var curdate = new Date(y, m, d)
+// console.log(curdate)
+d1 = curdate.getDate()
+m1 = curdate.getMonth();
+y1 = curdate.getFullYear();
+// console.log(d1,m1,y1)
+
+if(d1<10){
+    d1='0'+d1
+} 
+if(m1<10){
+    m1='0'+m1
+} 
+
+d2 = d1+1
+console.log(curdate)
+tommorow =y1+"-"+m1+"-"+d2;
+console.log(tommorow)
+document.querySelector(".arrivalDate").setAttribute("min",tommorow)
+// document.getElementsByClassName("arrivalDate").setAttribute("value",tommorow)
+
+// console.log(tommorow)
 
--- rezerwacja.js
+++ rezerwacja.js
@@ -13,11 +13,65 @@ const seat = document.querySelectorAll('.seat');
 document.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat')){
         e.target.classList.toggle('selected');
-        console.log(buttonValue)
     }
 }
 );
- selectedSeat(){
-     let chosenSeat = document.classList('selected')
-     
- }
+
+document.getElementsByClassName(".departureDate").innerHTML = localStorage.getItem("data wylotu");
+
+let departureDate = localStorage.getItem('data wylotu')
+
+console.log(departureDate)
+
+document.querySelector(".departureDate").innerHTML = departureDate;
+
+
+document.getElementsByClassName(".arrivalDate").innerHTML = localStorage.getItem("data powrotu");
+
+let arrivalDate = localStorage.getItem('data powrotu')
+
+console.log(arrivalDate)
+
+document.querySelector(".arrivalDate").innerHTML = arrivalDate;
+
+
+document.getElementsByClassName('.departureCity1').innerHTML = localStorage.getItem("depcity");
+
+let departureCityvalue = localStorage.getItem('depcity')
+
+document.querySelector(".departureCity").innerHTML = departureCityvalue;
+document.querySelector(".cityDeparture").innerHTML = departureCityvalue;
+
+
+document.getElementsByClassName('.arrivalCity1').innerHTML = localStorage.getItem("arrcity");
+
+let arrivalCityvalue = localStorage.getItem('arrcity')
+
+document.querySelector(".arrivalCity").innerHTML = arrivalCityvalue;
+document.querySelector(".cityArrival").innerHTML = arrivalCityvalue;
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + departureCityvalue +"&appid=16757cdb1c5e963cb82b1f47805831e7")
+  .then((resp) => resp.json()) // Transform the data into json
+  .then(function (data) {
+  //console.log(data.main.temp - 273)
+  let temperature = (data.main.temp-273).toFixed()
+  let weatherDepartureicon = (data.weather[0].icon)
+  document.getElementById("temperatureDeparture").innerHTML += `<div id="temperatureDeparture"> ${temperature} °C</div>`
+let iconDeparture = "http://openweathermap.org/img/w/" + weatherDepartureicon + ".png";
+  document.querySelector('.iconDeparture').src = "http://openweathermap.org/img/w/" + weatherDepartureicon + ".png"
+
+  })
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + arrivalCityvalue +"&appid=16757cdb1c5e963cb82b1f47805831e7")
+  .then((resp) => resp.json()) // Transform the data into json
+  .then(function (data) {
+  //console.log(data.main.temp - 273)
+  let temperature = (data.main.temp-273).toFixed()
+  let weatherId = (data.weather[0].icon)
+//   console.log(weatherId)
+  document.getElementById("temperatureArrival").innerHTML += `<div id="temperatureArrival"> ${temperature} °C</div>`;
+  let iconArrival = "http://openweathermap.org/img/w/" + weatherId + ".png";
+  document.querySelector('.iconArrival').src = "http://openweathermap.org/img/w/" + weatherId + ".png"
+})
+
